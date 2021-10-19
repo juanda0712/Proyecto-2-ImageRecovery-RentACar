212705d1fbce3b42867fde78d228b2a449e6a59c
eduardo <ebolivarminguet@gmail.com>
2021-10-18 18:49:01 -0600

    Configure QT

--- Image Recovery/main.cpp
+++ Image Recovery/main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <QApplication>
+#include <QPushButton>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+    QApplication a(argc, argv);
+    QPushButton button("Load image", nullptr);
+    button.resize(100,200);
+    button.show();
+    return QApplication::exec();
 }
