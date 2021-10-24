c46c2fc4e30abea3c238b6710ad14c51ed5d114a
juan <juanrm071299@gmail.com>
2021-10-23 18:38:00 -0600

    CMAKE configurado

--- Image Recovery/main.cpp
+++ Image Recovery/main.cpp
@@ -1,11 +1,18 @@
 #include <QApplication>
 #include "MyApplication.h"
 #include "genetic_algorithm/ImageCompleter.h"
+#include "opencv4/opencv2/opencv.hpp"
+
+using namespace cv;
 
 int main(int argc, char **argv) {
-    QApplication a(argc, argv);
+    /*QApplication a(argc, argv);
     MyApplication app;
     ImageCompleter genetics;
     printf("ULTIMO ID: %d\n", genetics.getConcreteIndividual(5).getId());
-    return QApplication::exec();
+    return QApplication::exec();*/
+
+    Mat image = imread("D:/My OpenCV Website/Eagle.jpg");
+    return 0;
+
 }
