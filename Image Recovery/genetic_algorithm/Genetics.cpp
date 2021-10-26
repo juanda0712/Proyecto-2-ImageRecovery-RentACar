8799c4f474165813feb2b9b139f6b1ea8fb6cd79
eduardo <ebolivarminguet@gmail.com>
2021-10-25 23:50:18 -0600

    Create Genetics.h Population.h and modify Individual.h

--- Image Recovery/genetic_algorithm/Genetics.cpp
+++ Image Recovery/genetic_algorithm/Genetics.cpp
@@ -15,23 +15,47 @@ Genetics::~Genetics() {
 }
 
 void Genetics::geneticAlgorithm() {
+    population.calculateFitness();
     while (this->numOfGenerations > 0) {
-
+        this->selection();
+        this->crossover();
+        if ((rand() + 1) % 6 < 5)) {
+            this->mutation();
+        }
+        this->addFittestChild();
+        population.calculateFitness();
     }
 }
 
 void Genetics::selection() {
-
+    this->fittest = population->getFittest();
+    this->secondFittest = population->getSecondFittest();
 }
 
 void Genetics::crossover() {
-
+    for (int i = 0; i < genes/2; i++) {
+        int temp = this->fittest.genes[i];
+        this->fittest.genes[i] = secondFittest.genes[i];
+        this->secondFittest.genes[i] = temp;
+    }
 }
 
 void Genetics::mutation() {
-
+    int mutationPoint;
+    if (this->fittest.genes[mutationPoint] == 1) {
+        this->fittest.genes[mutationPoint] = 0;
+    }
+    else {
+        this->fittest.genes[mutationPoint] = 1;
+    }
+    if (this->secondFittest.genes[mutationPoint] == 1) {
+        this->secondFittest.genes[mutationPoint] = 0;
+    }
+    else {
+        this->secondFittest.genes[mutationPoint] = 1;
+    }
 }
 
 Individual Genetics::getFittestChild() {
-
+    return this->fittest;
 }
--- Image Recovery/main.cpp
+++ Image Recovery/main.cpp
@@ -2,12 +2,18 @@
 #include "MyApplication.h"
 #include "genetic_algorithm/Genetics.h"
 #include "opencv4/opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
+using namespace std;
 using namespace cv;
 
 int main(int argc, char **argv) {
-    QApplication a(argc, argv);
-    //MyApplication app;
-    //Genetics genetics;
-    //return QApplication::exec();
+    Mat image = imread("/home/eduardo/Escritorio/Proyecto-2-ImageRecovery-RentACar/Image Recovery/images.png");
+    imshow("xd", image);
+    //cout << image.row(0) << endl;
+    //cout << image.col(0) << endl;
+    cout << image.rows << endl;
+    cout << image.row(1) << endl;
+    cout << image.row(0).col(0) << endl;
 }
