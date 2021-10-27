37a05befbf3c77109bec890bcb25a29e44b2806e
juan <juanrm071299@gmail.com>
2021-10-26 22:57:07 -0600

    puntos de la imagen faltante

--- Image Recovery/ImageOperations.h
+++ Image Recovery/ImageOperations.h
@@ -14,15 +14,18 @@ class ImageOperations {
 private:
     Mat image;
     vector<Vec3b> colorsList;
+    vector<int> points;
     int rows, cols, numberOfColors;
+    //Mat pixels
     int numberOfDifferentColors();
     void discardRedundantPixels();
 public:
     ImageOperations(Mat myImage);
     ~ImageOperations();
     void savePixels();
+    void rectanglePoints();
     vector<Vec3b> getColors();
+    vector<int> getPoints();
 };
-
 
 #endif //IMAGE_RECOVERY_IMAGEOPERATIONS_H
