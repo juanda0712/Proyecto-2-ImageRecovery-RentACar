fb74cb81e03f4b249a3db0d4f46350d918fa0d22
juan <juanrm071299@gmail.com>
2021-10-26 22:43:01 -0600

    puntos de la imagen faltante

--- Image Recovery/ImageOperations.cpp
+++ Image Recovery/ImageOperations.cpp
@@ -1,6 +1,3 @@
-//
-// Created by juanda on 10/26/21.
-//
 
 #include "ImageOperations.h"
 
@@ -27,4 +24,31 @@ void ImageOperations::savePixels() {
 
 vector <Vec3b> ImageOperations::getColors() {
     return this->colorsList;
+}
+
+void ImageOperations::rectanglePoints() {
+    for (int row = 0; row < this->image.rows; row++) {
+        for (int col = 0; col < this->image.cols; col++) {
+            Vec3b color = this->image.at<Vec3b>(Point(col,row));
+            if (color[0] == 255 & color[1] == 255 & color[2] == 255 ){
+                this->points.insert(points.cend(),col);
+                this->points.insert(points.cend(),row);
+                break;
+            }
+        }
+    }
+    for (int row =  this->image.rows-1; row  > 0 ; row--) {
+        for (int col = this->image.cols-1; col  > 0 ; col--) {
+            Vec3b color = this->image.at<Vec3b>(Point(col,row));
+            if (color[0] == 255 & color[1] == 255 & color[2] == 255 ){
+                this->points.insert(points.cend(),col);
+                this->points.insert(points.cend(),row);
+                break;
+            }
+        }
+    }
+}
+
+vector<int> ImageOperations::getPoints(){
+    return  this->points;
 }
--- Image Recovery/main.cpp
+++ Image Recovery/main.cpp
@@ -1,6 +1,5 @@
 #include <QApplication>
 #include "MyApplication.h"
-#include "genetic_algorithm/Genetics.h"
 #include "opencv4/opencv2/opencv.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
@@ -10,7 +9,6 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char **argv) {
-    Mat image = imread("/home/eduardo/Escritorio/Proyecto-2-ImageRecovery-RentACar/Image Recovery/images.png");
 
     ImageOperations op(image);
     op.savePixels();
