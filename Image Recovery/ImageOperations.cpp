64f7f4b4fe854237c8652609625087f92c2ba007
juan <juanrm071299@gmail.com>
2021-10-30 16:44:14 -0600

    agrego metodos pixelPercentage, quantities y modifico parametros de otros metodos

--- Image Recovery/ImageOperations.cpp
+++ Image Recovery/ImageOperations.cpp
@@ -5,17 +5,23 @@ ImageOperations::ImageOperations(Mat myImage) {
     this->totalImagePixels = myImage.rows * myImage.cols;
     this->rectanglePoints(myImage);
     this->totalRectanglePixels = (this->points[2]-this->points[0])*(this->points[3]-this->points[1]);
+    this->savePixels(myImage);
+
+    //llena de ceros el vector con el mismo tamano del vector colorLIst
+    for(int i = 0;  i < this->colorsList.size(); i++){
+        this->quantityOfEachPixel.insert(quantityOfEachPixel.cend(),0);
+    }
 }
 
 ImageOperations::~ImageOperations() {
 
 }
 
-void ImageOperations::savePixels() {
+void ImageOperations::savePixels(Mat myImage) {
     int i = 0;
-    for (int y = 0; y < this->image.rows; y++) {
-        for (int x = 0; x < this->image.cols; x++) {
-            Vec3b color = this->image.at<Vec3b>(Point(x,y));
+    for (int y = 0; y < myImage.rows; y++) {
+        for (int x = 0; x < myImage.cols; x++) {
+            Vec3b color = myImage.at<Vec3b>(Point(x,y));
             if (find(this->colorsList.begin(), this->colorsList.end(), color) == this->colorsList.end()) {
                 this->colorsList.push_back(color);
             }
@@ -31,8 +37,8 @@ vector <Vec3b> ImageOperations::getColors() {
 void ImageOperations::rectanglePoints(Mat myImage) {
     for (int row = 0; row < myImage.rows; row++) {
         for (int col = 0; col < myImage.cols; col++) {
-            Vec3b color = myImage.at<Vec3b>(Point(col,row));
-            if (color[0] == 255 & color[1] == 255 & color[2] == 255 ){
+            Vec3b pixel = myImage.at<Vec3b>(Point(col,row));
+            if (pixel[0] == 255 & pixel[1] == 255 & pixel[2] == 255 ){
                 this->points.insert(points.cend(),col);
                 this->points.insert(points.cend(),row);
                 break;
@@ -41,8 +47,8 @@ void ImageOperations::rectanglePoints(Mat myImage) {
     }
     for (int row =  myImage.rows-1; row  > 0 ; row--) {
         for (int col = myImage.cols-1; col  > 0 ; col--) {
-            Vec3b color = myImage.at<Vec3b>(Point(col,row));
-            if (color[0] == 255 & color[1] == 255 & color[2] == 255 ){
+            Vec3b pixel = myImage.at<Vec3b>(Point(col,row));
+            if (pixel[0] == 255 & pixel[1] == 255 & pixel[2] == 255 ){
                 this->points.insert(points.cend(),col);
                 this->points.insert(points.cend(),row);
                 break;
@@ -55,3 +61,26 @@ vector<int> ImageOperations::getPoints(){
     return  this->points;
 }
 
+void ImageOperations::quantities(Mat myImage){
+
+    for (int row = 0; row < myImage.rows; row++) {
+        for (int col = 0; col < myImage.cols; col++) {
+            Vec3b pixel = myImage.at<Vec3b>(Point(col,row));
+            for(int i = 0; i < this->colorsList.size(); i++){
+                Vec3b color = this->colorsList[i];
+                if (pixel[0] == color[0] & pixel[1] == color[1]  & pixel[2] == color[2]  ){
+                    this->quantityOfEachPixel[i] += 1;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void ImageOperations::pixelsPercentage(){
+    double percentage;
+    for (int i = 0; i < this->colorsList.size() ; ++i) {
+        percentage = (this->quantityOfEachPixel[i]/this->totalImagePixels)*100;
+        this->percentages.insert(percentages.cend(),percentage);
+    }
+}
--- Image Recovery/ImageOperations.h
+++ Image Recovery/ImageOperations.h
@@ -14,6 +14,8 @@ class ImageOperations {
 private:
     Mat image;
     vector<Vec3b> colorsList;
+    vector<int>  quantityOfEachPixel; // basado en colorsList , la primera posicion de colorsList esta relacionada con la primera posicion de este vector
+    vector<double> percentages;  //porcentajes de cada color basado en la posicion de colorList osea la primera posicion de colorsList esta relacionada con la primera posicion de este vector
     vector<int> points;
     int totalImagePixels, totalRectanglePixels;
     //Mat pixels
@@ -22,8 +24,10 @@ private:
 public:
     ImageOperations(Mat myImage);
     ~ImageOperations();
-    void savePixels();
+    void savePixels(Mat myImage);
     void rectanglePoints(Mat myImage);
+    void quantities(Mat myImage); // averigua la cantidad que hay de cada pixel (usa el vector quantityOfEachPixel)
+    void pixelsPercentage();
     vector<Vec3b> getColors();
     vector<int> getPoints();
 };
